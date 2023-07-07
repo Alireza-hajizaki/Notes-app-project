890f12987f6f4d2062c8a3a86d68e951e8eb20a9
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-07 15:18:22 +0330

    show note in dom

--- script.js
+++ script.js
@@ -8,50 +8,109 @@ const addBox = $.querySelector(".add-box"),
   buttonElem = $.querySelector("button"),
   wrapperElem = $.querySelector(".wrapper");
 
-  let notes = [];
-  let isUpdate = false;
+let notes = [];
+let isUpdate = false;
+
+addBox.addEventListener("click", () => {
+  if (isUpdate) {
+    popupTitle.innerHTML = "Update Note";
+    buttonElem.innerHTML = "update Note";
+  } else {
+    popupTitle.innerHTML = "Add a new Note";
+    buttonElem.innerHTML = "Add Note";
+  }
+
+  inputElem.focus();
+  popupBox.classList.add("show");
+});
 
 
-addBox.addEventListener('click' , () => {
+//closeing modal
 
-    if(isUpdate){
-        popupTitle.innerHTML = 'Update Note'
-        buttonElem.innerHTML = 'update Note'
-    }else{
-        popupTitle.innerHTML = 'Add a new Note'
-        buttonElem.innerHTML = 'Add Note'
-    }
+popupClose.addEventListener("click", closeModal);
 
-    inputElem.focus()
-    popupBox.classList.add('show')
-})
-
-popupClose.addEventListener('click' , () => {
-    console.log("close modal");
-})
-
-buttonElem.addEventListener('click' , () => {
-    console.log('add note');
-})
-
-function getLocalStorageNotes (){
-    let localStorageNotes = localStorage.getItem('notes');
-
-    if(localStorageNotes){
-        notes = JSON.parse(localStorageNotes)
-    }else{
-        notes = []
-    }
-
-    return notes;
+function closeModal(){
+    popupBox.classList.remove("show");
 }
 
-function generateNotes (notes){
-console.log(notes);
+buttonElem.addEventListener("click", () => {
+  const newNote = {
+    title: inputElem.value,
+    description: textareaElem.value,
+    date: "april 2023 12",
+  };
+
+  notes.push(newNote);
+  setNotesInLocalStorage(notes);
+  closeModal()
+  generateNotes(notes)
+  clearInputs();
+});
+
+function clearInputs() {
+    inputElem.value = ''
+    textareaElem.value = ''
 }
 
-window.addEventListener('load' , () => {
-    let notes = getLocalStorageNotes()
-    generateNotes(notes)
-})
+function getLocalStorageNotes() {
+  let localStorageNotes = localStorage.getItem("notes");
 
+  if (localStorageNotes) {
+    notes = JSON.parse(localStorageNotes);
+  } else {
+    notes = [];
+  }
+
+  return notes;
+}
+
+function generateNotes(notes) {
+
+    //delet all note in dom
+    $.querySelectorAll('.note').forEach(note => note.remove())
+
+  notes.forEach((note) => {
+    wrapperElem.insertAdjacentHTML(
+      "beforeend",
+      `
+        <li class="note">
+        <div class="details">
+          <p>${note.title}</p>
+          <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+          <span>${note.date}</span>
+          <div class="settings">
+            <i class="uil uil-ellipsis-h" onclick="showSetting(this)"></i>
+            <ul class="menu">
+              <li onclick="editNote({index}, '{note.title}', '{note.description}')">
+                <i class="uil uil-pen"></i>Edit
+              </li>
+              <li onclick="removeNote({index})">
+                <i class="uil uil-trash"></i>Delete
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+        `
+    );
+  });
+}
+
+function showSetting (el){
+    el.parentElement.classList.add('show')
+}
+
+function setNotesInLocalStorage(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+window.addEventListener("load", () => {
+  let notes = getLocalStorageNotes();
+  generateNotes(notes);
+});
+
+window.addEventListener('keyup' , (e) => {
+  if(e.key==='Escape') closeModal();
+})
